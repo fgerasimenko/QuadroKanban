a72249c46b888556c72f3e60b2ff1a1ae89d495e
Fabio Gerasimenko Pinheiro <fabio.pinheiro@confitec.com.br>
2026-01-30 20:49:46 -0300

    feat: add task movement functionality and enhance TaskCard UI

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -23,17 +23,8 @@ export function TaskCard({ task, onClick, onMove }: Props) {
                 <span className={`pill pill-${priorityLower}`}>
                     {priorityLabel(task.priority)}
                 </span>
-            </div>
 
-            <div className="card-meta">
-                <div><strong>Data:</strong> {dateToString(task.createdAt)}</div>
-                <div><strong>Data Final:</strong> {dateToString(task.deadline)}</div>
-                <div><strong>Prioridade:</strong>{priorityLabel(task.priority)}</div>
-                <div><strong>Local:</strong> {task.location}</div>
-                <div>{task.shortDescription}</div>
-            </div>
-
-            {onMove && (
+                {onMove && (
                 <div className="card-actions" onClick={(e) => e.stopPropagation()}>
                 {task.status !== "TODO" && (
                     // <i className="fa-solid fa-chevron-left" onClick={() => onMove("TODO")}></i>
@@ -51,6 +42,34 @@ export function TaskCard({ task, onClick, onMove }: Props) {
                 )}
                 </div>
             )}
+            </div>
+
+            <div className="card-meta">
+                <div><strong>Data:</strong> {dateToString(task.createdAt)}</div>
+                <div><strong>Data Final:</strong> {dateToString(task.deadline)}</div>
+                <div><strong>Prioridade:</strong>{priorityLabel(task.priority)}</div>
+                <div><strong>Local:</strong> {task.location}</div>
+                <div>{task.shortDescription}</div>
+            </div>
+
+            {/* {onMove && (
+                <div className="card-actions" onClick={(e) => e.stopPropagation()}>
+                {task.status !== "TODO" && (
+                    // <i className="fa-solid fa-chevron-left" onClick={() => onMove("TODO")}></i>
+                    <button className="icon-btn" onClick={() => onMove("TODO")}>
+                        <i className="fa-solid fa-chevron-left"></i>
+                    </button>
+                )}
+                {task.status !== "DONE" && (
+                    <button
+                        className="icon-btn"
+                        onClick={() => onMove(task.status === "TODO" ? "DOING" : "DONE")}
+                    >
+                    <i className="fa-solid fa-chevron-right"></i>
+                    </button>
+                )}
+                </div>
+            )} */}
         </div>        
     )
 
--- src/data/mock.ts
+++ src/data/mock.ts
@@ -57,5 +57,24 @@ export const mockTasks: Task[] = [
             name: "Usuário"
         },
         archived: false
+    },
+    {
+        id: "4",
+        order: 4,
+        title: "Título grande pra cacete que faz não caber o pill na mesma linha que o proprio titulo",
+        longDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        location: "Projeto",
+        isExternal: false,
+        priority: 'CRITICAL',
+        status: 'TODO',
+        deadline: new Date("2026-03-04"),
+        createdAt: new Date("2026-01-01"),
+        updatedAt: new Date("2026-01-01"),
+        archivedAt: undefined,
+        assignedTo: {
+            id: "1",
+            name: "Usuário"
+        },
+        archived: false
     }
 ]
